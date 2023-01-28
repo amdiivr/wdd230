e7b5337f118090b827f6ef4279e7f6b687211cdd
amdiivr <amdiivr@gmail.com>
2023-01-28 18:10:53 -0500

    add

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,13 @@
+/* HAMBURGER MENU*/
+
+let nav = document.querySelector(".navigation");
+let button = document.getElementById("menu");
+
+
+button.addEventListener("click", () => {nav.classList.toggle("responsive")}, false);
+
+/* ACTUALLY DATE TO HEADER*/
+
 let dateNow = document.querySelector(".date");
 
 let now = new Date();
@@ -7,3 +17,11 @@ let fullDate = new Intl.DateTimeFormat("en-UK", {
 }).format(now);
 
 dateNow.innerHTML = `<p>${fullDate}</p>`;
+
+/* ACTUALLY DATE FOOTER */
+
+let year = new Date().getFullYear();
+document.querySelector("#year").innerHTML = year;
+
+let lastUpdate = new Date(document.lastModified);
+document.querySelector("#currentDate").innerHTML = lastUpdate;
