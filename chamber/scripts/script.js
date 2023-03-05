7a379d9e5b88f70b12c80ab88ff78f1f7c6f19ec
amdiivr <amdiivr@gmail.com>
2023-03-04 23:43:01 -0500

    add changes

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -45,18 +45,3 @@ if (dayOfTheWeek[day] == "Monday" | dayOfTheWeek[day] == "Tuesday") {
         alertBanner.remove();    
 });
 }
-
-// WEATHER 
-
-let windSpeed = 21;
-let temperature = 84.2;
-
-function CalculateWindChill(speed, temp) {
-    return 35.74 + .621*temp - (35.75*speed**.16) + .4275*temp*speed**.16;
-}
-
-document.querySelector("#temperature").textContent = temperature;
-document.querySelector("#speedWind").textContent = windSpeed;
-document.querySelector("#chillWind").textContent = CalculateWindChill(windSpeed, temperature).toFixed(1);
-
-
