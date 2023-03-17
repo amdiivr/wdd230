4d33ff4272995ce166ff20441a4780c6fb54a929
amdiivr <amdiivr@gmail.com>
2023-03-17 10:31:38 -0500

    add changes

--- chamber/scripts/visited.js
+++ chamber/scripts/visited.js
@@ -1,35 +1,58 @@
-const todayDisplay = document.querySelector("#today");
-const visitsDisplay = document.querySelector("#visits");
+// const todayDisplay = document.querySelector("#today");
+// const visitsDisplay = document.querySelector("#visits");
 
-let numVisits = Number(window.localStorage.getItem("visits-ls")); 
+// let numVisits = Number(window.localStorage.getItem("visits-ls")); 
 
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit!`;
-}
+// if (numVisits !== 0) {
+// 	visitsDisplay.textContent = numVisits;
+// } else {
+// 	visitsDisplay.textContent = `This is your first visit!`;
+// }
 
+// numVisits++;
+
+// localStorage.setItem("visits-ls", numVisits);
+
+// const visitInfo = document.querySelector("#today");
+
+// let lastVisit = Number(window.localStorage.getItem("last-visit"));
+
+// if (lastVisit !== 0) {
+//     let daysMilSeconds = Date.now() - lastVisit;
+//     let days = (daysMilSeconds / 84600000).toFixed(0);
+
+//     if (days == 1) {
+//         visitInfo.textContent = `Your last visit was ${days} day ago! Welcome again!`;
+//     }
+//     else if (days == 0) {
+//         visitInfo.textContent = "Your last visit was today!"
+//     }
+// }
+// else {
+//     visitInfo.textContent = "This is your first visit! Welcome!";
+// }
+
+// localStorage.setItem("last-visit", Date.now())
+
+const visitsDisplay = document.getElementById("visits");
+const daysBetweenOutput = document.getElementById("today");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let lastTime = Number(window.localStorage.getItem("lastVisitDateTime"));
 numVisits++;
-
+if (numVisits > 1){
+    visitsDisplay.textContent = numVisits;
+}else{
+    visitsDisplay.textContent = 'This is your first visit!';
+    lastTime = Date.now();
+}
+let daysBetween = (Date.now() - lastTime)/86400000;
 localStorage.setItem("visits-ls", numVisits);
+localStorage.setItem("lastVisitDateTime", Date.now());
 
-const visitInfo = document.querySelector("#today");
-
-let lastVisit = Number(window.localStorage.getItem("last-visit"));
-
-if (lastVisit !== 0) {
-    let daysMilSeconds = Date.now() - lastVisit;
-    let days = (daysMilSeconds / 84600000).toFixed(0);
-
-    if (days == 1) {
-        visitInfo.textContent = `Your last visit was ${days} day ago! Welcome again!`;
-    }
-    else if (days == 0) {
-        visitInfo.textContent = "Your last visit was today!"
-    }
+let rounded = Math.round(daysBetween);
+if (rounded !==0){
+    daysBetweenOutput.textContent= rounded;
+}else{
+    daysBetweenOutput.textContent = "It hasn't been a day yet";
 }
-else {
-    visitInfo.textContent = "This is your first visit! Welcome!";
-}
-
-localStorage.setItem("last-visit", Date.now())
